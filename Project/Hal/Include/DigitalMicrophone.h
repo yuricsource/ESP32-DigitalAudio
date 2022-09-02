8fc42cda2da7191b568ea0520875ed1fa32db64c
Yuri R <yuri.flash@gmail.com>
2022-09-02 23:47:58 +1000

    Improving digital microphone.

--- Project/Hal/Include/DigitalMicrophone.h
+++ Project/Hal/Include/DigitalMicrophone.h
@@ -2,77 +2,30 @@
 #define HAL_DIGITAL_MIC_H_
 
 #include "driver/i2s.h" 
+#include "HalCommon.h"
 
 
-// you shouldn't need to change these settings
-#define SAMPLE_BUFFER_SIZE 1024*2
-#define SAMPLE_RATE 16000
-// most microphones will probably default to left channel but you may need to tie the L/R pin low
-#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
-// either wire your microphone to the same pins or change these to match your wiring
-#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_2
-#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_13
-#define I2S_MIC_SERIAL_DATA GPIO_NUM_26
-#define PIXEL_SIZE 4
 
-// // don't mess around with this
-// i2s_config_t i2s_config = {
-//     .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
-//     .sample_rate = SAMPLE_RATE,
-//     .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-//     .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-//     .communication_format = I2S_COMM_FORMAT_I2S,
-//     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-//     .dma_buf_count = 4,
-//     .dma_buf_len = 1024,
-//     .use_apll = false,
-//     .tx_desc_auto_clear = false,
-//     .fixed_mclk = 0};
-
-// // and don't mess around with this
-// i2s_pin_config_t i2s_mic_pins = {
-//     .bck_io_num = I2S_MIC_SERIAL_CLOCK,
-//     .ws_io_num = I2S_MIC_LEFT_RIGHT_CLOCK,
-//     .data_out_num = I2S_PIN_NO_CHANGE,
-//     .data_in_num = I2S_MIC_SERIAL_DATA};
-
-// static const char *TAG = "main";
-
-
-// i2s_config_t i2sSpeakerConfig = {
-//     .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-//     .sample_rate = SAMPLE_RATE,
-//     .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-//     .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-//     .communication_format = I2S_COMM_FORMAT_I2S,
-//     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-//     .dma_buf_count = 4,
-//     .dma_buf_len = 1024};
-
-// // i2s pins
-// i2s_pin_config_t i2sSpeakerPins = {
-//     .bck_io_num = GPIO_NUM_27,
-//     .ws_io_num = GPIO_NUM_12,
-//     .data_out_num = GPIO_NUM_25,
-//     .data_in_num = -1};
-
-//     int16_t raw_samples[SAMPLE_BUFFER_SIZE];
 
 namespace Hal
 {
     class DigitalMicrophone
     {
         public:
-            DigitalMicrophone(uint32_t sampleRate = DefaultSampleRate, i2s_port_t i2sPort = I2S_NUM_0);
+            DigitalMicrophone(gpio_num_t bkPin, gpio_num_t wsPin, gpio_num_t dataPin, uint32_t sampleRate = DefaultSampleRate, i2s_port_t i2sPort = I2S_NUM_0);
             ~DigitalMicrophone();
-            uint32_t GetAudioBuffer(uint16_t* buffer, size_t bufferSize, TickType_t timeout = portMAX_DELAY);
+            size_t GetAudioBuffer(int16_t* buffer, size_t bufferSize, TickType_t timeout = portMAX_DELAY);
             bool Start();
             void Stop();
         private:
             static constexpr uint32_t DefaultSampleRate = 16000; 
             i2s_config_t _config = {};
             i2s_port_t _i2sPort = I2S_NUM_0;
+            i2s_pin_config_t _i2sPins = {};
             bool _initialized = false;
+
+        private:
+        // delete &DigitalMicrophone;
     };
 }
 
--- Project/Hal/Source/DigitalMicrophone.cpp
+++ Project/Hal/Source/DigitalMicrophone.cpp
@@ -1,22 +1,90 @@
 #include "DigitalMicrophone.h"
+#ifdef DEBUG    
+#include <hagl_hal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
+#include <esp_log.h>
+
+#include <font6x9.h>
+#include <aps.h>
+#include <fps.h>
+#include <hagl.h>
+#include "sdkconfig.h"
+
+#define TAG "MIC"
+#define SAMPLE_BUFFER_SIZE 1024
+#define PIXEL_SIZE 4
+#endif
 
 namespace Hal
 {
-
-    DigitalMicrophone::DigitalMicrophone(uint32_t sampleRate, i2s_port_t i2sPort):
+    DigitalMicrophone::DigitalMicrophone(gpio_num_t bkPin, gpio_num_t wsPin, gpio_num_t dataPin,
+                                            uint32_t sampleRate, i2s_port_t i2sPort):
     _i2sPort(i2sPort)
     {
+        // Configuring the I2S driver
         _config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX);
         _config.sample_rate = sampleRate;
         _config.bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT;
         _config.channel_format = I2S_CHANNEL_FMT_ONLY_LEFT;
-        _config.communication_format = I2S_COMM_FORMAT_I2S;
+        _config.communication_format = I2S_COMM_FORMAT_STAND_I2S;
         _config.intr_alloc_flags = ESP_INTR_FLAG_LEVEL1;
         _config.dma_buf_count = 4;
         _config.dma_buf_len = 1024;
         _config.use_apll = false;
         _config.tx_desc_auto_clear = false;
         _config.fixed_mclk = 0;
+
+        // Configuring the pins
+        _i2sPins.bck_io_num = bkPin;
+        _i2sPins.ws_io_num = wsPin;
+        _i2sPins.data_out_num = I2S_PIN_NO_CHANGE;
+        _i2sPins.data_in_num = dataPin;
+
+        // Initializing the I2S driver
+        i2s_driver_install(_i2sPort, &_config, 0, NULL);
+        i2s_set_pin(_i2sPort, &_i2sPins);
+
+#ifdef DEBUG
+        color_t green = hagl_color(0, 255, 0);
+        ESP_LOGI(TAG, "Heap when starting: %d", esp_get_free_heap_size());
+        hagl_init();
+        ESP_LOGI(TAG, "Heap after HAGL init: %d", esp_get_free_heap_size());
+
+        static int16_t raw_samples[SAMPLE_BUFFER_SIZE];
+
+        while(true)
+        {
+            size_t bytes_read = 0;
+            bytes_read = GetAudioBuffer(raw_samples, sizeof(int16_t) * SAMPLE_BUFFER_SIZE);
+            int samples_read = bytes_read / sizeof(int16_t);
+            
+            int16_t pData = DISPLAY_WIDTH / 2;
+            uint8_t pY = 0;
+            uint32_t unitStep = SAMPLE_BUFFER_SIZE / DISPLAY_HEIGHT;
+
+            for (uint16_t i = 0; i < bytes_read; i ++)
+            {
+                raw_samples[i] = raw_samples[i] / 8;
+            }
+                                        // 240
+            for (uint16_t y = 0; y < DISPLAY_HEIGHT; y += 1) 
+            {
+                int16_t data = (raw_samples[y * unitStep]) + DISPLAY_WIDTH / 2;
+                hagl_draw_line(pData, pY, data, y, green);
+                pData = data;
+                pY = y;
+            }
+            hagl_flush();
+            hagl_clear_screen();
+            vTaskDelay(1);
+        }
+#endif
     }
 
     DigitalMicrophone::~DigitalMicrophone()
@@ -24,10 +92,10 @@ namespace Hal
 
     }
 
-    uint32_t DigitalMicrophone::GetAudioBuffer(uint16_t* buffer, size_t bufferSize, TickType_t timeout)
+    size_t DigitalMicrophone::GetAudioBuffer(int16_t* buffer, size_t bufferSize, TickType_t timeout)
     {
         size_t bytes_read = 0;
-        i2s_read(I2S_NUM_0, buffer, bufferSize, &bytes_read, timeout);
+        i2s_read(_i2sPort, buffer, bufferSize, &bytes_read, timeout);
         return bytes_read / sizeof(int16_t);
     }
 
@@ -36,7 +104,7 @@ namespace Hal
         if (_initialized)
             return true;
 
-        _initialized = (ESP_OK == i2s_driver_install(I2S_NUM_0, &_config, 0, NULL));
+        _initialized = (ESP_OK == i2s_driver_install(_i2sPort, &_config, 0, NULL));
 
         return _initialized;
     }
@@ -47,7 +115,7 @@ namespace Hal
             return;
 
         _initialized = false;
-        i2s_driver_install(I2S_NUM_0, &_config, 0, NULL);
+        i2s_driver_install(_i2sPort, &_config, 0, NULL);
     }
 
 }

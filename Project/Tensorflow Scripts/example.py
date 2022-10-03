18cc2f7162cf89e64f19e7b7f2f9960ef446bec3
Yuri_Linux <yuri.flash@gmail.com>
2022-10-04 00:30:48 +1100

    Python script - Merging 2 different types tensors with 2 different sizes

--- Project/Tensorflow Scripts/example.py
+++ Project/Tensorflow Scripts/example.py
@@ -7,6 +7,7 @@ import sys
 import utils.Log as l;
 import numpy as np
 from scipy.io.wavfile import write
+import torch
 
 audioPath = 'Tensorflow Scripts/speech_data/Luna/'
 file1 = "Luna.wav"
@@ -17,18 +18,31 @@ if not os.path.exists(audioPath):
     exit()
 
 # creating a empty tensor
-tensor = tf.reshape(tf.convert_to_tensor(()), (0, 16752))
+tensor = torch.empty(16000, 1)
 
 
 audio1 = tfio.audio.AudioIOTensor(audioPath + file1)
 audio2 = tfio.audio.AudioIOTensor(audioPath + file2)
 
-print("\n\n\n\n\n")
 
+# tensor = tensor.numpy() + np.resize(audio1[0:].numpy(), [16000,1])
+def sum(a, b):
+    if len(a) < len(b):
+        c = b.copy()
+        c[:len(a)] += a
+    else:
+        c = a.copy()
+        c[:len(b)] += b
+    return c
+
+tensor =  sum(tfio.audio.AudioIOTensor(audioPath + file2)[0:].numpy(), tfio.audio.AudioIOTensor(audioPath + file1)[0:].numpy())
+
+
+print("\n\n\n\n\n")
+l.log("Empty Tensor Size", tensor)
 l.log("Tensor Type", type(audio1[100:][0]))
 l.log("Array Size", audio1[100:].get_shape()[0])
 
-tensor = audio1[100:].numpy()
 
 # Ploting two audios in the same plot
 plt.figure()
